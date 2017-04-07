99909f511e0ab66283616b60fc6058822dbebaec
Dieter Menne <dieter.menne@menne-biomed.de>
2017-04-07 15:59:18 +0200

    Finished plot.breathtestfit

--- R/plot_breathtestfit.R
+++ R/plot_breathtestfit.R
@@ -5,29 +5,37 @@
 #' @title S3 plot method for breathtestfit
 #' @param x object of class breathtestfit, as returned by nls_fit or nlme_fit
 #' @param inc increment for fitted curve plot in minutes
+#' @param method_t50 method for t50: \code{maes_ghoos}, \code{bluck_coward} or 
+#' \code{maes_ghoos_scint}
 #' @param ... other parameters passed to methods
 #' @importFrom graphics plot
 
-plot.breathtestfit = function(x, inc = 5, ...){
+plot.breathtestfit = function(x, inc = 5, method_t50 = "maes_ghoos", ...){
   # Make CRAN happy
   pdr = parameter = value = method = minute = fitted = group = NULL
   # use library(broom) for debugging of augment
-  dd = augment(x, by = inc) 
+  dd = broom::augment(x, by = inc) 
+  # Mark t50
+  t50 = coef(x) %>%
+    filter(parameter == "t50",  method == method_t50) %>% 
+    select(-parameter)
   # Avoid ugly ggplot shading
   theme_set(theme_bw() + theme(panel.spacing = grid::unit(0,"lines")))
   if (length(unique(dd$group)) > 1) {
-    p = ggplot(dd, aes(x = minute, y = fitted, color = group)) + geom_line() +
-      geom_point(data = x$data, aes(x = minute, y = pdr, color = group)) 
+    p = ggplot(dd, aes(x = minute, y = fitted, color = group)) + geom_line(size = 1) +
+      geom_point(data = x$data, aes(x = minute, y = pdr, color = group), size = 1) 
+    if (nrow(t50) > 0 )    
+      p = p + geom_vline(aes(xintercept = value, color = group),  t50) 
   } else {
-    p = ggplot(dd, aes(x = minute, y = fitted)) + geom_line() +
-      geom_point(data = x$data, aes(x = minute, y = pdr)) 
+    p = ggplot(dd, aes(x = minute, y = fitted)) + geom_line(size = 1) +
+      geom_point(data = x$data, aes(x = minute, y = pdr), size = 1) 
+    if (nrow(t50) > 0 )    
+      geom_vline(aes(xintercept = value, color = "red" ),  t50) 
   } 
-  # Mark t50
-  t50 = coef(x) %>% 
-    filter(parameter == "t50") %>% 
-    select(-parameter)
-  p +  
-    geom_vline(aes(xintercept = value, color = method), size = 1, t50) +
-    facet_wrap(~patient_id) 
+  p + facet_wrap(~patient_id) +
+    scale_colour_brewer(type = "seq", palette = "Set1") + 
+    ylab("pdr") +
+    ggtitle(label = NULL, subtitle = paste("Half-emptying t50 by method", method_t50))
 }
+
 
--- tests/testthat/test_plot_breathtestfit.R
+++ tests/testthat/test_plot_breathtestfit.R
@@ -7,10 +7,10 @@ nlayers <- function(x) length(ggplot_build(x)$data)
 test_that("Plot layers match expectations",{
   # With seed = 100, rec_10 fails nls-fit
   data = cleanup_data(simulate_breathtest_data(seed = 100)$data)
-  fit = nls_fit(data)
-  p = plot(fit)
+  x = nls_fit(data)
+  p = plot(x)
   expect_gt(max(layer_data(p)$y), 25)
-  expect_equal(nlayers(p), 3)   
+  expect_equal(nlayers(p), 2)   
   expect_equal(length(p), 9)   
   expect_equal(nlevels(layer_data(p)$PANEL), 10)
 })
@@ -21,8 +21,8 @@ test_that("Plot multiple groups",{
     A = simulate_breathtest_data(n_records = 6, seed = 100)$data,
     B = simulate_breathtest_data(n_records = 4, seed = 187)$data 
   )
-  fit = nls_fit(cleanup_data(data))
-  p = plot(fit)
+  x = nls_fit(cleanup_data(data))
+  p = plot(x)
   expect_equal(nlayers(p), 3)   
   expect_equal(length(p), 9)   
   expect_equal(nlevels(layer_data(p)$PANEL), 6)
