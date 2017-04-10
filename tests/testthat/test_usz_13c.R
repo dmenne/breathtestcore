89f8b8a883403c58c3ad19a5f1b9c50116096541
Dieter Menne <dieter.menne@menne-biomed.de>
2017-04-10 11:00:12 +0200

    Disambiguated patient_id in  usz_13c

--- tests/testthat/test_usz_13c.R
+++ tests/testthat/test_usz_13c.R
@@ -3,11 +3,11 @@ context("Data")
 test_that("USZ data have the correct columns",{
   data("usz_13c")  
   expect_equal(names(usz_13c), c("patient_id","group","minute","pdr"))
-  expect_identical(nrow(usz_13c), 15168L)
+  expect_identical(nrow(usz_13c), 15561L)
   expect_match(comment(usz_13c), "Zurich")
   # Check that there are no duplicates
   expect_identical(nrow(usz_13c[duplicated(usz_13c[,-4]),]),0L)
   nls_bad = attr(usz_13c, "nls_bad")
   # check for one known case
-  expect_match(nls_bad, "pat_012")
+  expect_match(nls_bad, "pat_013")
 })
