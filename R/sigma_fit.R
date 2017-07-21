4154e4d775c5f40c1a4872f4accf50a3b3812545
Dieter Menne <dieter.menne@menne-biomed.de>
2017-07-21 17:33:58 +0200

    Corrected syntax in documentation

--- R/sigma_fit.R
+++ R/sigma_fit.R
@@ -1,8 +1,9 @@
 #' @title S3 method to exctract the residual error
-#' @description Functions for `nls ànd `nlme` are available; additional functions
-#' for Stan-based fits are defined in package `breathteststan`. 
-#' @param fit Result of class `breathtestfit
+#' @description Functions for \code{nls} and \code{nlme} are available; 
+#' additional functions for Stan-based fits are defined in package \code{breathteststan}. 
+#' @param fit Result of class \code{breathtestfit}
 #' @return A numeric value giving the standard deviation of the residuals.
+#' @importFrom stats residuals var
 #' @export 
 sigma_fit = function(fit) {
   UseMethod("sigma_fit")
