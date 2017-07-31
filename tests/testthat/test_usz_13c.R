aa0a30571631b4eb72aad1bd25b12d2d727bfe15
Dieter Menne <dieter.menne@menne-biomed.de>
2017-07-31 10:20:50 +0200

    Added 2 new data sets with tests

--- tests/testthat/test_usz_13c.R
+++ tests/testthat/test_usz_13c.R
@@ -11,3 +11,23 @@ test_that("USZ data have the correct columns",{
   # check for one known case
   expect_match(nls_bad, "pat_013")
 })
+
+
+test_that("USZ 13c a data have the correct columns",{
+  data("usz_13c_a")  
+  expect_equal(names(usz_13c_a), c("patient_id","group","minute","pdr"))
+  # Check that there are no duplicates
+  expect_identical(nrow(usz_13c_a[duplicated(usz_13c_a[,-4]),]),0L)
+  library(dplyr)
+  d = usz_13c_a %>% 
+       cleanup_data() %>% 
+       nlme_fit()
+  expect_match(comment(d$data),"no successful fit with nlme")
+})
+
+test_that("USZ 13c d data have the correct columns",{
+  data("usz_13c_d")  
+  expect_equal(names(usz_13c_d), c("patient_id","group","minute","pdr"))
+  # Check that there are no duplicates
+  expect_identical(nrow(usz_13c_d[duplicated(usz_13c_d[,-4]),]),0L)
+})
