42bee7b2a920c7db3889276b85a3fcf8716d1a30
Dieter Menne <dieter.menne@menne-biomed.de>
2020-07-14 09:26:06 +0200

    Version 0.8

--- check_rhub.cmd
+++ check_rhub.cmd
@@ -1,7 +1,3 @@
-rem rscript -e "rhub::check(platform = "debian-clang-devel")
-rscript -e "rhub::check_on_ubuntu()"
-rscript -e "rhub::check_with_rdevel()"
-rscript -e "rhub::check_on_macos()"
-rscript -e "rhub::check_on_solaris()"
+rscript -e "rhub::check_for_cran()"
 ECHO "Window will be closed after 5 seconds"
 @PING -n 5 127.0.0.1>nul 
