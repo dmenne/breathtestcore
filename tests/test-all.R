4aa15071076406d0036b90b9281421931b0b1352
Dieter Menne <dieter.menne@menne-biomed.de>
2024-11-21 09:01:15 +0100

    Removed parallely from test_all

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,6 +1,6 @@
 library(testthat)
 
-options(Ncpus = parallelly::availableCores(omit = 1))
+#options(Ncpus = parallelly::availableCores(omit = 1))
 #options(Ncpus = 1)
-#options(lifecycle_verbosity = "warning")
+options(lifecycle_verbosity = "warning")
 test_check("breathtestcore")
