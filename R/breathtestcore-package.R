893b9c8b69b3d64475fff9bd848553b2d76806ab
Dieter Menne <dieter.menne@menne-biomed.de>
2024-11-22 17:46:44 +0100

    Using _PACKAGE

---
+++ R/breathtestcore-package.R
@@ -0,0 +1,12 @@
+#' @title breathtestcore
+#' @description Stan fits to 13C breath test curves. Reads several formats 
+#'   of 13C   data (IRIS/Wagner, BreathID) and CSV.  Creates artificial 
+#'   sample data for testing.  Fits Maes/Ghoos, Bluck-Coward self-correcting
+#'   formula using 'nls', 'nlme'. Methods to fit breath test curves with
+#'   Bayesian Stan methods are refactored to package 'breathteststan'. 
+#'   For a Shiny GUI, see package 'dmenne/breathtestshiny' on github.
+#' 
+#' @keywords internal
+#' "_PACKAGE"
+#' @name breathtestcore-package
+#' @aliases breathtestcore
