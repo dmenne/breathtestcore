0f09a9db1123c113048e934b3454d6ef9931ea34
Dieter Menne <dieter.menne@menne-biomed.de>
2026-07-13 09:22:18 +0200

    Version 0.8.11. Explicit set warn=0 to silence Fedora build

--- tests/test-all.R
+++ tests/test-all.R
@@ -3,4 +3,5 @@ library(testthat)
 #options(Ncpus = parallelly::availableCores(omit = 1))
 #options(Ncpus = 1)
 options(lifecycle_verbosity = "warning")
+options(warn = 0) # Try to silence problems with Fedora build July 2026
 test_check("breathtestcore")
